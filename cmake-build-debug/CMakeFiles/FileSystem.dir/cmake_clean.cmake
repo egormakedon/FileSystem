3384f68c4cf2ffc7ba9291902c9c7a3809cc6ea2
egor <egor.krutoychel.komp@mail.ru>
2017-12-12 02:14:32 +0300

    init

---
+++ cmake-build-debug/CMakeFiles/FileSystem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FileSystem.dir/main.cpp.o"
+  "FileSystem.pdb"
+  "FileSystem"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FileSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
